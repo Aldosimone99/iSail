dd502ac76bba2ba9be2629b171ae6939996a4030
Aldosimone99 <aldosimonedirosa@gmail.com>
2025-02-25 19:37:09 +0100

    fixed course page

--- lib/screens/course_detail_screen.dart
+++ lib/screens/course_detail_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
 import 'dart:io'; // Import for File class
+import 'package:flutter/cupertino.dart'; // Import for CupertinoActionSheet
 import '../models/course.dart';
 
 class CourseDetailScreen extends StatefulWidget {
@@ -35,6 +36,56 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
         _pdfPath = result.files.single.path;
       });
     }
+  }
+
+  void _showPickerOptions(BuildContext context) {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return Stack(
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.of(context).pop(),
+              child: Container(
+                color: Colors.black54, // Semi-transparent gray background
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SafeArea(
+                child: CupertinoActionSheet(
+                  title: Text('Carica il corso', style: TextStyle(color: Colors.white)), // White text color
+                  actions: <Widget>[
+                    CupertinoActionSheetAction(
+                      child: Text('Seleziona dalla Galleria', style: TextStyle(color: Colors.white)), // White text color
+                      onPressed: () {
+                        _pickImage();
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    CupertinoActionSheetAction(
+                      child: Text('Seleziona da File', style: TextStyle(color: Colors.white)), // White text color
+                      onPressed: () {
+                        _pickPDF();
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                  cancelButton: CupertinoActionSheetAction(
+                    child: Text('Annulla', style: TextStyle(color: Colors.white)), // White text color
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   Color _getCircleColor() {
@@ -102,45 +153,40 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
             style: TextStyle(fontSize: 24, color: Colors.grey[300], fontWeight: FontWeight.bold), // Increase the font size, set color to light gray, and make bold
           ),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.image),
-            onPressed: _pickImage,
-          ),
-          IconButton(
-            icon: Icon(Icons.picture_as_pdf),
-            onPressed: _pickPDF,
-          ),
-        ],
       ),
       body: SingleChildScrollView( // Wrap the Column in SingleChildScrollView
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the left
             children: [
               SizedBox(height: 50), // Add space above the circle
-              Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  color: _getCircleColor(),
-                  shape: BoxShape.circle,
-                  border: Border.all(color: _getBorderColor(), width: 3), // Add border
-                ),
-                child: Center(
-                  child: Text(
-                    _getDaysRemaining(),
-                    style: TextStyle(fontSize: 24, color: _getTextColor(), fontWeight: FontWeight.bold), // Use the same text color
-                  ),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Il corso scadrà tra:',
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8), // Add space between the text and the circle
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: _getCircleColor(),
+                        shape: BoxShape.circle,
+                        border: Border.all(color: _getBorderColor(), width: 3), // Add border
+                      ),
+                      child: Center(
+                        child: Text(
+                          _getDaysRemaining(),
+                          style: TextStyle(fontSize: 24, color: _getTextColor(), fontWeight: FontWeight.bold), // Use the same text color
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 16),
-              Text(
-                'Scadenza:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text(DateFormat('dd/MM/yyyy').format(widget.course.deadline)),
               SizedBox(height: 16),
               if (_imageFile != null)
                 Image.file(File(_imageFile!.path)),
@@ -155,6 +201,12 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showPickerOptions(context),
+        child: Icon(Icons.add, color: Colors.black), // Black icon color
+        backgroundColor: Colors.white, // White background color
+        shape: CircleBorder(), // Ensure the button is round
       ),
     );
   }
